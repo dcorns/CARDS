f4caa23142ca26bf089e81f671c6c25b89d51e43
Dale Corns <drcorns@hotmail.com>
2014-02-19 00:43:03 -0800

    [66037598] open git with first card files

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint:{
+      all:['*.js']
+    },
+    simplemocha: {
+      options: {
+        timeout: 3000,
+        ignoreLeaks: true,
+        reporter: 'tap'
+      },
+
+      all: { src: ['test/**/*.js'] }
+    }
+  });
+
+  // For this to work, you need to have run `npm install grunt-simple-mocha`
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Add a default task. This is optional, of course :)
+  grunt.registerTask('default', ['simplemocha','jshint']);
+
+};
